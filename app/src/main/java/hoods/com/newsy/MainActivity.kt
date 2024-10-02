0082b0d1af224bb2457849014c6fa5c2ded2f7ca
CodingRock-Star <dhirajdafouti2020@gmail.com>
2024-10-02 17:57:35 +0530

    Configure changes

--- app/src/main/java/hoods/com/newsy/MainActivity.kt
+++ app/src/main/java/hoods/com/newsy/MainActivity.kt
@@ -13,34 +13,34 @@ import androidx.compose.ui.tooling.preview.Preview
 import hoods.com.newsy.ui.theme.NewsyTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            NewsyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      NewsyTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+          modifier = Modifier.fillMaxSize(),
+          color = MaterialTheme.colorScheme.background
+        ) {
+          Greeting("Android")
         }
+      }
     }
+  }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+  Text(
+    text = "Hello $name!",
+    modifier = modifier
+  )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    NewsyTheme {
-        Greeting("Android")
-    }
+  NewsyTheme {
+    Greeting("Android")
+  }
 }
